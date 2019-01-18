df0b60cf440376d6c089f36a91ac244236574a35
stbaeumer <baeumer@posteo.de>
2019-01-18 18:44:35 +0100

    ...

--- server.js
+++ server.js
@@ -16,49 +16,54 @@ const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
-// Die Funktion soll auswerten, ob jemand angemeldet ist oder nicht.
-// Wenn jemand angemeldet ist, dann wird 1 zurückgegeben, ansonsten 0.
-// Der Cookie wird als Key-Value-Paar als Parameter in die Funktion gegeben.
+const mysql = require('mysql')
+const dbVerbindung = mysql.createConnection({
+    host: "130.255.124.99", 
+    user: "placematman", 
+    password: "BKB123456!", 
+    database: "dbpaedtag"
+})
 
-function istAngemeldet(cookie){   // cookie = { istAngemeldet: '0' } oder { istAngemeldet: '1' }
-    
-    // Der Value des Keys "istAngemeldet" wird ausgelesen.
+dbVerbindung.connect()
 
-    let cookieNullEinsString = cookie["istAngemeldet"]   // "0" oder "1"
-    
-    // Der Value "1" wird in die Ganzahl 1 umgewandelt.
-    // Der Value "0" wird in die Ganzahl 0 umgewandelt.
-
-    let cookieNullEinsInt = parseInt(cookieNullEinsString)  // 0 oder 1 bzw. false oder true
-
-    console.log("istAngemeldet: " + cookie["istAngemeldet"])
-    
-    // Es wird die Ziffer 0 oder 1 zurückgegeben.
-    // 0 hat den boolschen Wert false.
-    // 1 hat den boolschen Wert true.
-
-    return cookieNullEinsInt     
+function istAngemeldetAls(cookie){   // cookie = { istAngemeldetAls: '' } oder { istAngemeldetAls: 'BM' }
+    return cookie["istAngemeldetAls"]     
 }
 
 // Die app.get wird aufgerufen, sobald die Webseite aufgerufen wird.
 
-app.get('/',(req, res, next) => { 
-    
-    // Es wird geprüft, ob der Anwender bereits angemeldet ist.
+app.get('/',(req, res, next) => {     
 
-    if(istAngemeldet(req.cookies)){        
+    console.log("::")
+    console.log(req.cookies)
+    console.log(istAngemeldetAls(req.cookies))
+    console.log("::")
 
-        // Falls der Anwender bereits angemeldet ist, wird er begrüßt.
+    if(istAngemeldetAls(req.cookies)){        
 
-        res.render('index.ejs', {                    
-            anzeigen: ["Sie sind angemeldet.", "Herzlich willkommen!"]
-        })
+        let lehrerKrz = istAngemeldetAls(req.cookies)
+
+        dbVerbindung.query("SELECT * from lehrer WHERE lehrerkrz = '" + lehrerKrz + "';", (err, rows) => { 
+            
+            if (err) return next(err)       
+            
+            if(rows[0] === undefined){
+                console.log("Cookies werden gelöscht")
+                res.cookie('istAngemeldetAls', '')              
+                res.render('anmelden.ejs', {                    
+                    anzeigen: ["Es kann Lehrer mit dem Kürzel '" + lehrerKrz + "' und der von Ihnen eingetippten PIN gefunden werden!", "Versuchen Sie es erneut!", "Die PIN sollte Ihnen per E-Mail zugegagnen sein."],                
+                })
+            }else{                
+                res.render('index.ejs', {                    
+                    anzeigen: ["Sie sind erfolgreich angemeldet!", "Gut gemacht, " + lehrerKrz + "!", "Wie Sie das immer wieder hinkriegen!"],                
+                })
+            }        
+        })   
     }else{
-
-        // Falls der Anwender nicht angemeldet ist, wird er zur 'anmelden.ejs'-Seite
-        // weitergeleitet.
-
-        res.render('anmelden.ejs', {            
+        console.log("Cookies werden gelöscht ...")
+        res.cookie('istAngemeldetAls', '')
+        res.render('anmelden.ejs', {  
+            anzeigen : ["Bitte melden Sie sich an!", "Die Zugangsdaten sind Ihnen per E-Mail zugegangen.", "Bei Fragen fragen: <a href='mailto:stefan.baeumer@berufskolleg-borken.de?Subject=Hallo' target='_top'>Stefan Bäumer</a>"]          
         })
     }
 })
@@ -66,24 +71,160 @@ app.get('/',(req, res, next) => {
 // Die app.post wird aufgerufen, sobald der Button im Formular geklickt wird.
 
 app.post('/',(req, res, next) => {    
-    if(req.body.password === "123"){    
-        res.cookie('istAngemeldet', 1)    
-        res.render('index.ejs', {                    
-            anzeigen: ["Erfolgreich eingeloggt!"]        
-        })    
+    
+    let lehrerKrz = req.body.lehrerKrz
+    let pin = req.body.pin
+    
+    dbVerbindung.query("SELECT * from lehrer WHERE lehrerkrz = '" + lehrerKrz + "' AND pin = " + pin + ";", (err, rows) => { 
+        if (err) return next(err)       
+        
+		if(rows[0] === undefined){    
+            console.log("Cookies werden gelöscht ....")                
+            res.cookie('istAngemeldetAls', '')            
+            res.render('anmelden.ejs', {                    
+                anzeigen: ["Es kann Lehrer mit dem Kürzel '" + lehrerKrz + "' und der von Ihnen eingetippten PIN gefunden werden!", "Versuchen Sie es erneut!", "Die PIN sollte Ihnen per E-Mail zugegagnen sein."],                
+            })
+        }else{     
+            console.log("User wird jetzt angemeldet als: " + lehrerKrz)       
+            res.cookie('istAngemeldetAls', lehrerKrz)            
+            res.render('index.ejs', {                    
+                anzeigen: ["Sie sind erfolgreich angemeldet!", "Gut gemacht, " + lehrerKrz + "!", "Wie Sie das immer wieder hinkriegen!"],                
+            })
+        }        
+    })   
+})
+
+app.post('/anmelden',(req, res, next) => {                
+    res.cookie('istAngemeldetAls', '')    
+    res.render('anmelden.ejs', {                    
+        anzeigen: ["Abgemeldet.", "Geben Sie Ihr Kürzel und die PIN ein, die Ihnen per E-Mail zugegangen ist."]        
+    })    
+})
+
+app.get('/anmelden',(req, res, next) => {                
+    res.cookie('istAngemeldetAls', '')    
+    res.render('anmelden.ejs', {                    
+        anzeigen: ["Abgemeldet.", "Geben Sie Ihr Kürzel und die PIN ein, die Ihnen per E-Mail zugegangen ist."]        
+    })    
+})
+
+app.get('/tagesablauf',(req, res, next) => {                
+    
+    let tagesablauf = []
+
+    tagesablauf.push("<b>08:30 - 09:00 Uhr Eingangsreferat</b>")
+    tagesablauf.push("<b>09:45 - 10:45 Uhr 1.Workshop<b>")
+    //tagesablauf.push("Sie müssen sich ab Mitte Februar für den 1.Workshop Ihrer Wahl amelden!")
+    tagesablauf.push("<b>11:00 - 12:00 Uhr 2.Workshop<b>")
+    //tagesablauf.push("Sie müssen sich ab Mitte Februar für den 2.Workshop Ihrer Wahl amelden!")
+    tagesablauf.push("<b>12:15 - 13:00 Mittagspause<b>")
+    //tagesablauf.push("Der Festausschuss ...")
+    tagesablauf.push("<b>13:15 - 14:15 Anwendungsphase<b>")
+    //tagesablauf.push("Geplant ist, dass sich wiederum jeder einer Gruppe zuordnet. Die Gruppen sich nach Fächern / Bereichen vorgesehen. Falls zwei Gruppen geeignet erscheinen, dann frei auswählen.")
+    tagesablauf.push("<b>13:15 - 14:15 Plenum<b>")
+
+    res.render('tagesablauf.ejs', {                    
+        anzeigen: tagesablauf
+    })    
+})
+
+app.get('/workshops',(req, res, next) => {                
+
+    let workshops = []
+
+    workshops.push("<h2>Workshop 1 Padlet</h2><p>Hallo <a href='http://www.padlet.com/' target='_blank'>Padlet</a>  </p>")
+    workshops.push("<h2>Workshop 2 Padlet</h2><p>Hallo <a href='http://www.padlet.com/' target='_blank'>Padlet</a>  </p>")
+    res.render('workshops.ejs', {                    
+        anzeigen: workshops
+    })    
+})
+
+app.get('/auswahl1',(req, res, next) => {                
+    
+    if(istAngemeldetAls(req.cookies)){        
+        res.render('auswahl1.ejs', { 
+            anzeigen : ["Sie müssen auswähen!"]                               
+        })
     }else{
-        res.cookie('istAngemeldet', 0)
-        res.render('anmelden.ejs', {                                        
+        res.render('anmelden.ejs', {            
         })
     }
 })
 
-app.post('/abmelden',(req, res, next) => {                
-    res.cookie('istAngemeldet', 0)    
-    res.render('anmelden.ejs', {                    
-        anzeigen: "Angemeldet"        
-    })    
+app.get('/auswahl2',(req, res, next) => {                
+    
+    if(istAngemeldetAls(req.cookies)){        
+        res.render('auswahl2.ejs', { 
+            anzeigen : ["Sie müssen auswähen!"]                               
+        })
+    }else{
+        res.render('anmelden.ejs', {            
+        })
+    }
 })
+
+app.get('/auswahl3',(req, res, next) => {                
+    
+    if(istAngemeldetAls(req.cookies)){        
+        res.render('auswahl3.ejs', { 
+            anzeigen : ["Sie müssen auswähen!"]                               
+        })
+    }else{
+        res.render('anmelden.ejs', {            
+        })
+    }
+})
+
+
+
+app.post('/auswahl1',(req, res, next) => {        
+    if(istAngemeldetAls(req.cookies)){      
+        let anzeigen = []
+        anzeigen.push("Hallo")
+        
+        res.render('auswahl1.ejs', {                                
+            anzeigen : anzeigen
+        })
+    }else{
+        res.render('anmelden.ejs', {            
+        })
+    }
+})
+
+app.post('/auswahl2',(req, res, next) => {                
+    
+    if(istAngemeldetAls(req.cookies)){      
+
+        let anzeigen = []
+        
+        res.render('auswahl2.ejs', {                                
+            anzeigen : anzeigen
+        })
+    }else{
+        res.render('anmelden.ejs', {            
+        })
+    }
+})
+
+app.post('/auswahl3',(req, res, next) => {                
+    
+    if(istAngemeldetAls(req.cookies)){      
+
+        let anzeigen = []
+        anzeigen.push("Hallo")
+        
+        res.render('auswahl3.ejs', {                                
+            anzeigen : anzeigen
+        })
+    }else{
+        res.render('anmelden.ejs', {            
+        })
+    }
+})
+
+
+
+
 
 app.use((err, req, res, next) => {    
     console.log(err.stack)
